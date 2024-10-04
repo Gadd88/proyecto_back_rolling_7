974e4dd08eb4ef9bb98d2fe77fc74616b60521ab
Matias Saade <matiassaade@gmail.com>
2024-10-03 23:48:15 -0300

    fix: arreglada autenticacion para crud de productos

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -8,18 +8,25 @@ const {
   traerTodosProductos,
   traerUnProducto,
 } = require("../controllers/product-controller.js");
+const checkAuth = require("../middlewares/auth.js");
 
 const router = express.Router();
 
 router.get("/products", traerTodosProductos);
 router.get("/products/:product_id", traerUnProducto);
-router.post("/products", uploadFile.single("imagen"), crearProducto);
+router.post(
+  "/products",
+  checkAuth("admin"),
+  uploadFile.single("imagen"),
+  crearProducto
+);
 router.patch(
   "/products/:product_id",
+  checkAuth("admin"),
   uploadFile.single("imagen"),
   actualizarUnProducto
 );
-router.delete("/products/:product_id", eliminarProducto);
+router.delete("/products/:product_id", checkAuth("admin"), eliminarProducto);
 router.get("/products/category/:category", traerProductosPorCategoria);
 //router.post('/products', checkAuth('admin'));
 //router.patch('/products/:product_id', checkAuth('admin'))
